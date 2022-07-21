a4c8e6658ec221f2016367d84582368c973faca2
dhruv50ae <linkdp2012@gmail.com>
2022-07-21 14:56:10 +0100

    KWARGS

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ def add(*args):
 
 sum = add(1,1,2,3,4,5,6,6,7,8,2)
 
-print(sum)
+# print(sum)
+
+def calculator(n, **kwargs):
+    # print(kwargs)
+    # for key, values in kwargs.items():
+    #     print(key)
+    #     print(values)
+    n+=kwargs["add"]
+    n*=kwargs["multiply"]
+    return n
+
+total = calculator(1, add = 2, multiply = 5)
+print(total)
